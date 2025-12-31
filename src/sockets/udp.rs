022f84b15499548d0d66fe91b32e0de28e0080cc
Ilya Shishov <ilya.shishov@engee.cloud>
2025-12-31 14:52:22 +0300

    UDP socket workable implementation + echo loopback test

--- src/sockets/udp.rs
+++ src/sockets/udp.rs
@@ -1,71 +1,132 @@
-use crate::sock::{SimpleSock, Socket};
+use crate::serde_helpers;
+use crate::sock::{SimpleSock, SimpleSockBlock, SockBlockCtl, SocketFactory};
 use serde::Deserialize;
 use std::collections::HashMap;
-use std::io::{Error, ErrorKind};
+use std::io::{self, Error, ErrorKind};
 use std::net::{IpAddr, UdpSocket};
 
-fn d_ip_local() -> IpAddr {
+/// Default local IP address.
+fn default_ip_local() -> IpAddr {
     "0.0.0.0".parse().unwrap()
 }
-fn d_port_local() -> u16 {
+
+/// Default local port.
+fn default_port_local() -> u16 {
     0
 }
+
+/// Configuration for UDP socket.
 #[derive(Deserialize)]
 struct UdpConfig {
-    #[serde(default = "d_ip_local")]
+    #[serde(default = "default_ip_local")]
     ip_local: IpAddr,
     ip_dst: IpAddr,
-    #[serde(default = "d_port_local")]
+    #[serde(
+        default = "default_port_local",
+        deserialize_with = "serde_helpers::string_to_u16"
+    )]
     port_local: u16,
+    #[serde(deserialize_with = "serde_helpers::string_to_u16")]
     port_dst: u16,
 }
 
+/// Simple UDP socket implementation.
 struct SimpleUDP {
-    conf: UdpConfig,
-    sock: UdpSocket,
+    _config: UdpConfig, // Prefix with underscore to suppress unused warning
+    socket: UdpSocket,
 }
 
 impl SimpleSock for SimpleUDP {
-    fn read(&self, data: &mut [u8], _: usize) -> std::io::Result<usize> {
-        self.sock.recv(data)
+    fn read(&self, data: &mut [u8], _sz: usize) -> io::Result<usize> {
+        // In kind of empty socket we want Ok(0) to return
+        match self.socket.recv(data) {
+            Err(err) => {
+                if err.kind() == ErrorKind::WouldBlock {
+                    return Ok(0);
+                }
+                return Err(err);
+            }
+            count => count,
+        }
     }
-    fn write(&self, data: &[u8], sz: usize) -> std::io::Result<()> {
-        self.sock.send(data[..sz].as_ref())?;
+
+    fn write(&self, data: &[u8], sz: usize) -> io::Result<()> {
+        self.socket.send(&data[..sz])?;
         Ok(())
     }
 }
 
-pub struct SocketUDP {
-    simple_udp: Option<Box<dyn SimpleSock>>,
-}
-
-impl SocketUDP {
-    pub fn new() -> Self {
-        Self { simple_udp: None }
+impl SockBlockCtl for SimpleUDP {
+    fn set_block(&self, is_block: bool) -> io::Result<()> {
+        // Invert the operation
+        self.socket.set_nonblocking(!is_block)
     }
 }
 
-impl Socket for SocketUDP {
-    fn create_sock(&self, params: HashMap<String, String>) -> std::io::Result<Box<dyn SimpleSock>> {
-        let udp_cfg =
-            match serde_json::from_value::<UdpConfig>(match serde_json::to_value(params) {
-                Ok(val) => val,
-                Err(_) => return Err(Error::from(ErrorKind::InvalidInput)),
-            }) {
-                Err(_) => return Err(Error::from(ErrorKind::InvalidInput)),
-                Ok(udp_cfg) => udp_cfg,
-            };
-        let sock = UdpSocket::bind(format!("{}:{}", udp_cfg.ip_local, udp_cfg.port_local))?;
-        sock.connect(format!("{}:{}", udp_cfg.ip_dst, udp_cfg.port_dst))?;
+/// UDP socket factory implementing the SocketFactory trait.
+pub struct SocketFactoryUDP;
+
+impl SocketFactoryUDP {
+    /// Creates a new UDP socket factory.
+    pub fn new() -> Self {
+        Self
+    }
+}
+
+impl SocketFactory for SocketFactoryUDP {
+    fn create_sock(&self, params: HashMap<String, String>) -> io::Result<Box<dyn SimpleSockBlock>> {
+        // Convert params to JSON value
+        let json_value = serde_json::to_value(params)
+            .map_err(|_| Error::new(ErrorKind::InvalidInput, "Invalid parameters"))?;
+
+        // Deserialize to UdpConfig
+        let udp_config: UdpConfig = serde_json::from_value(json_value).map_err(|e| {
+            eprintln!("{e}");
+            Error::new(ErrorKind::InvalidInput, "Invalid UDP configuration")
+        })?;
+
+        // Bind and connect the socket
+        let socket = UdpSocket::bind(format!("{}:{}", udp_config.ip_local, udp_config.port_local))?;
+        socket.connect(format!("{}:{}", udp_config.ip_dst, udp_config.port_dst))?;
+
+        // Set the nonblocking by default
+        socket.set_nonblocking(true)?;
+
         Ok(Box::new(SimpleUDP {
-            conf: udp_cfg,
-            sock,
+            _config: udp_config,
+            socket,
         }))
     }
-    fn get_simple_sock(&self) -> &dyn SimpleSock {
-        self.simple_udp.as_ref().unwrap().as_ref()
-    }
-    fn set_simple_sock(&mut self, simple_sock: Box<dyn SimpleSock>) {
-        self.simple_udp = Some(simple_sock);
+}
+
+mod tests {
+    use super::*;
+    use crate::test_helpers::*;
+
+    #[test]
+    fn test_udp_socket_echo_loopback() {
+        let factory = SocketFactoryUDP::new();
+        let mut sender_params = HashMap::new();
+        let mut receiver_params = HashMap::new();
+        let port_sender = "8081";
+        let port_receiver = "8080";
+        let snd_data = vec!['H', 'e', 'l', 'l', 'o'];
+
+        sender_params.insert("ip_dst".to_string(), "127.0.0.1".to_string());
+        sender_params.insert("port_dst".to_string(), port_receiver.to_string());
+        sender_params.insert("port_local".to_string(), port_sender.to_string());
+
+        receiver_params.insert("ip_dst".to_string(), "127.0.0.1".to_string());
+        receiver_params.insert("port_dst".to_string(), port_sender.to_string());
+        receiver_params.insert("port_local".to_string(), port_receiver.to_string());
+
+        assert!(
+            if let Err(e) = echo_loopback_test(&factory, sender_params, receiver_params, snd_data) {
+                eprintln!("{e}");
+                false
+            } else {
+                true
+            }
+        )
     }
 }
