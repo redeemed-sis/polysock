d3b4e2a3afac32f6630b43cd40bc4b8201562067
Ilya Shishov <ilya.shishov@engee.cloud>
2026-01-05 13:07:27 +0300

    Add MIT license

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
+/*
+ * Copyright (c) 2026 Ilya Shishov
+ * Licensed under the MIT License.
+ * See the LICENSE file in the project root for full license information.
+ */
+
 use std::io;
 
 mod sockets;
